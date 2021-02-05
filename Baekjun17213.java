566d8d49719cf3887ce3479be33b9f2f9917da65
sjsjsjghkdwp <58241854+sjsjsjghkdwp@users.noreply.github.com>
2021-02-05 16:38:36 +0900

    Add files via upload

---
+++ Baekjun17213.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjun17213 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int r = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
+		// 문제 : (n-r)H(r) = (n-1)C(r-1)
+		System.out.println(combination(n-1,r-1));
+	}
+
+	private static int combination(int i, int j) {
+		if(j==0 || i==j) {
+			return 1;
+		}
+		return i*combination(i-1,j-1)/j;
+	}
+}
